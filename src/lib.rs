b085f89aca92aa57718a3411a030d90afcf9208c
non7247 <funi_funi@nifty.com>
2021-09-26 11:49:45 +0900

    Point::origin()、Vector::x_axis()、Vector::y_axis()、Vector::z_axis()を追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+const DEFAULT_TOLERANCE_CALCULATION: f64 = 1.0e-8;
+const DEFAULT_TOLERANCE_CONVERGENCE: f64 = 1.0e-6;
+const DEFAULT_TOLERANCE_POINT: f64 = 1.0e-4;
+const DEFAULT_TOLERANCE_VECTOR: f64 = 1.0e-6;
+
 enum ErrorStatus {
     Ok,
     InvalidInput,
@@ -18,11 +23,29 @@ mod geo {
         pub z: f64,
     }
 
+    impl Point {
+        fn origin() -> Point {
+            Point { x: 0.0, y: 0.0, z: 0.0 }
+        }
+    }
+
     #[derive(Debug)]
     struct Vector {
         pub x: f64,
         pub y: f64,
         pub z: f64,
+    }
+
+    impl Vector {
+        fn x_axis() -> Vector {
+            Vector { x: 1.0, y: 0.0, z: 0.0 }
+        }
+        fn y_axis() -> Vector {
+            Vector { x: 0.0, y: 1.0, z: 0.0 }
+        }
+        fn z_axis() -> Vector {
+            Vector { x: 0.0, y: 0.0, z: 1.0 }
+        }
     }
 }
 
