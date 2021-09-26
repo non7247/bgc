11406e1f080fff3fd1da364666ef5bd89e2326bf
non7247 <funi_funi@nifty.com>
2021-09-26 10:11:41 +0900

    Point、Vectorの定義を追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
+enum ErrorStatus {
+    Ok,
+    InvalidInput,
+    OutOfRange,
+    MustBePositive,
+    MustBeNoNegative,
+    MustBeNonZero,
+    Deivergence,
+    Empty,
+    NotInplemented,
+}
+
+mod geo {
+    #[derive(Debug)]
+    struct Point {
+        pub x: f64,
+        pub y: f64,
+        pub z: f64,
+    }
+
+    #[derive(Debug)]
+    struct Vector {
+        pub x: f64,
+        pub y: f64,
+        pub z: f64,
+    }
+}
+
+mod math {
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
