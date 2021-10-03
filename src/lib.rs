6332a03f54e625d7b935c3b6036f0b15826aa178
non7247 <funi_funi@nifty.com>
2021-10-03 09:45:54 +0900

    Pointにdistance_to()とis_equal_to()を追加

--- src/lib.rs
+++ src/lib.rs
@@ -17,33 +17,47 @@ enum ErrorStatus {
 
 mod geo {
     #[derive(Debug)]
-    struct Point {
+    pub struct Point {
         pub x: f64,
         pub y: f64,
         pub z: f64,
     }
 
     impl Point {
-        fn origin() -> Point {
+        pub fn origin() -> Point {
             Point { x: 0.0, y: 0.0, z: 0.0 }
+        }
+
+        pub fn distance_to(&self, rhs: &Self) -> f64 {
+            let dx = self.x - rhs.x;
+            let dy = self.y - rhs.y;
+            let dz = self.z - rhs.z;
+
+            (dx * dx + dy * dy + dz * dz).sqrt()
+        }
+
+        pub fn is_equal_to(&self, rhs: &Self) -> bool {
+            self.distance_to(rhs) < crate::DEFAULT_TOLERANCE_POINT
         }
     }
 
     #[derive(Debug)]
-    struct Vector {
+    pub struct Vector {
         pub x: f64,
         pub y: f64,
         pub z: f64,
     }
 
     impl Vector {
-        fn x_axis() -> Vector {
+        pub fn x_axis() -> Vector {
             Vector { x: 1.0, y: 0.0, z: 0.0 }
         }
-        fn y_axis() -> Vector {
+
+        pub fn y_axis() -> Vector {
             Vector { x: 0.0, y: 1.0, z: 0.0 }
         }
-        fn z_axis() -> Vector {
+
+        pub fn z_axis() -> Vector {
             Vector { x: 0.0, y: 0.0, z: 1.0 }
         }
     }
@@ -54,8 +68,13 @@ mod math {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn point_is_equal_to() {
+        let lhs = geo::Point { x: 1.0, y: 2.0, z: 3.0 };
+        let rhs = geo::Point { x: 1.0, y: 2.0, z: 3.0 };
+
+        assert!(lhs.is_equal_to(&rhs));
     }
 }
