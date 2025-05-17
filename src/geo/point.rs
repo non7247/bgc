e45604f99f1f6c342881d8674bd635a4be13cae3
non7247 <funi_funi@nifty.com>
2025-05-17 16:41:15 +0900

    fix: #4 Matrix3dの定義の変更に合わせてPoint.transformを修正

--- src/geo/point.rs
+++ src/geo/point.rs
@@ -40,22 +40,37 @@ impl Point {
 
     /// Transforms this point to the coordinate system of the transformation matrix
     ///
-    /// \[p\] * \[M\] = \[p'\]
-    pub fn transform(&self, rhs: &Matrix3d) -> Self {
-        let x = self.x * rhs.get(0, 0)
-            + self.y * rhs.get(1, 0)
-            + self.z * rhs.get(2, 0)
-            + rhs.get(3, 0);
-        let y = self.x * rhs.get(0, 1)
-            + self.y * rhs.get(1, 1)
-            + self.z * rhs.get(2, 1)
-            + rhs.get(3, 1);
-        let z = self.x * rhs.get(0, 2)
-            + self.y * rhs.get(1, 2)
-            + self.z * rhs.get(2, 2)
-            + rhs.get(3, 2);
+    /// \[M\] * \[p\] = \[p'\]
+    pub fn transform(&self, rhs: &Matrix3d, tol: &Tolerance) -> Result<Self, BgcError> {
+        let x_prime = rhs.get(0, 0) * self.x
+            + rhs.get(0, 1) * self.y
+            + rhs.get(0, 2) * self.z
+            + rhs.get(0, 3) * 1.0;
 
-        Self::new(x, y, z)
+        let y_prime = rhs.get(1, 0) * self.x
+            + rhs.get(1, 1) * self.y
+            + rhs.get(1, 2) * self.z
+            + rhs.get(1, 3) * 1.0;
+
+        let z_prime = rhs.get(2, 0) * self.x
+            + rhs.get(2, 1) * self.y
+            + rhs.get(2, 2) * self.z
+            + rhs.get(2, 3) * 1.0;
+
+        let w_prime = rhs.get(3, 0) * self.x
+            + rhs.get(3, 1) * self.y
+            + rhs.get(3, 2) * self.z
+            + rhs.get(3, 3) * 1.0;
+
+        if w_prime.abs() < tol.calculation() {
+            Err(BgcError::MustBeNonZero)
+        } else {
+            Ok(Self::new(
+                x_prime / w_prime,
+                y_prime / w_prime,
+                z_prime / w_prime
+            ))
+        }
     }
 }
 
@@ -243,26 +258,33 @@ mod tests {
 
         let mut mat = Matrix3d::new();
         mat.set(0, 0, 0.9238);
-        mat.set(0, 1, 0.2514);
-        mat.set(0, 2, 0.1113);
-        mat.set(0, 3, 0.0);
-        mat.set(1, 0, 0.2156);
+        mat.set(0, 1, 0.2156);
+        mat.set(0, 2, -0.5856);
+        mat.set(0, 3, 7.2);
+        mat.set(1, 0, 0.2514);
         mat.set(1, 1, -0.1132);
-        mat.set(1, 2, -0.4286);
-        mat.set(1, 3, 0.0);
-        mat.set(2, 0, -0.5856);
-        mat.set(2, 1, 0.3126);
+        mat.set(1, 2, 0.3126);
+        mat.set(1, 3, 3.6);
+        mat.set(2, 0, 0.1113);
+        mat.set(2, 1, -0.4286);
         mat.set(2, 2, 0.8156);
-        mat.set(2, 3, 0.0);
-        mat.set(3, 0, 7.2);
-        mat.set(3, 1, 3.6);
-        mat.set(3, 2, 4.0);
+        mat.set(2, 3, 4.0);
+        mat.set(3, 0, 0.0);
+        mat.set(3, 1, 0.0);
+        mat.set(3, 2, 0.0);
         mat.set(3, 3, 1.0);
 
-        let transformed = p.transform(&mat);
-        assert!(transformed.is_equal_to(
-                &Point::new(17.5229, 7.0419, 5.1788),
-                &Tolerance::default()
-        ));
+        let transformed = p.transform(&mat, &Tolerance::default());
+        match transformed {
+            Ok(p) => {
+                assert!(p.is_equal_to(
+                    &Point::new(17.5229, 7.0419, 5.1788),
+                    &Tolerance::default()
+                ));
+            },
+            Err(error) => {
+                panic!("error in point_transform: {:?}", error);
+            },
+        }
     }
 }
